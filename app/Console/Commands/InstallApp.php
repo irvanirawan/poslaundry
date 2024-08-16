d89f19f36a1c0e0d3106ab59e6799e983b56402c
irvanirawanit <irvanirawanit@gmail.com>
2024-08-16 15:38:39 +0700

    membuat command install diawal

---
+++ app/Console/Commands/InstallApp.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'run migrate seed and serve';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate:fresh');
+        $this->call('db:seed');
+        $this->call('serve');
+
+        $this->info('Application installed successfully');
+
+        return 0;
+    }
+}
